07e533b40c18943977e53601ac723b4f37bb7d0b
masiljangajji <hjk172262@gmail.com>
2023-09-17 23:44:50 +0900

    refactor: 문제 요구사항에 맞게 변경

--- src/main/java/org/nhnacademy/lsj/Problem1.java
+++ src/main/java/org/nhnacademy/lsj/Problem1.java
@@ -6,11 +6,17 @@ import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 1부터 지정된 최대값까지의 범위에 있는 여러 가지 임의의 정수를 포함하는 ArrayList를 생성하는 서브루틴을 작성하세요 .
+ */
 public class Problem1 {
 
 
     private static final Logger logger = LoggerFactory.getLogger(Problem1.class);
 
+    /**
+     * 프로그램 진행.
+     */
     public static void problem1() {
 
         Scanner sc = new Scanner(System.in);
@@ -24,12 +30,18 @@ public class Problem1 {
         }
     }
 
+    /**
+     * 1~max까지의 수를 무작위로 입력.
+     *
+     * @param max 최대 범위.
+     * @return List.
+     */
     public static List<Integer> rangeInput(int max) {
 
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 1; i <= max; i++) {
-            list.add(i);
+        for (int i = 1; i <= 30; i++) {
+            list.add((int) (Math.random() * max + 1));
         }
         return list;
 
