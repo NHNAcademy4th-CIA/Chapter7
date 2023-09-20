f0717937eaf72bf0abf9a534ab359c62cfcc2b56
minm063 <jangmin063@gmail.com>
2023-09-20 03:12:05 -0700

    refactor : 메소드로 분리

--- src/main/java/org/nhnacademy/minju/Exercise3.java
+++ src/main/java/org/nhnacademy/minju/Exercise3.java
@@ -5,16 +5,29 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * .custom 정렬과 Arrays.sort의 각각 걸리는 시간을 비교하는 프로그램
+ */
 public class Exercise3 {
     private static final Logger logger = LoggerFactory.getLogger(Exercise3.class);
 
+    /**
+     * .충분히 큰 배열에 대해 정렬을 진행한다.
+     */
     public static void exercise3() {
+
+        int[] array = new int[2_147];
+        makeArray(array);
+
+        calcTimeInSelectionSort(array);
+        calcTimeInArraySort(array);
+    }
+
+    private static void calcTimeInSelectionSort(int[] array) {
         long startTime;
         long endTime;
         long compTime;
         double seconds;
-        int[] array = new int[2_147];
-        makeArray(array);
 
         startTime = System.nanoTime();
         selectionSort(array);
@@ -23,10 +36,16 @@ public class Exercise3 {
         seconds = compTime / 1000000000.0;
         logger.info("Run time in nanoseconds was: {}", compTime);
         logger.info("Run time in seconds was: {}", seconds);
+    }
+
+    private static void calcTimeInArraySort(int[] array) {
+        long startTime;
+        long endTime;
+        long compTime;
+        double seconds;
 
         startTime = System.nanoTime();
         Arrays.sort(array);
-        selectionSort(array);
         endTime = System.nanoTime();
         compTime = endTime - startTime;
         seconds = compTime / 1000000000.0;
