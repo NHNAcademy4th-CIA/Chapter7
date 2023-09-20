ee91ebc602a097b8368ee2cbf98f1324610fc43a
minm063 <jangmin063@gmail.com>
2023-09-20 03:12:54 -0700

    refactor : TreeMap으로 변경, sort 삭제

--- src/main/java/org/nhnacademy/minju/Exercise6.java
+++ src/main/java/org/nhnacademy/minju/Exercise6.java
@@ -3,9 +3,10 @@ package org.nhnacademy.minju;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +23,7 @@ public class Exercise6 {
      * sort arrayList
      */
     public static void exercise6() {
-        HashSet<String> set = new HashSet<>();
+        Map<String, Integer> map = new TreeMap<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/temp.txt"))) {
             String row;
@@ -39,29 +40,30 @@ public class Exercise6 {
                             sb.append(ch);
                         }
                     }
-                    set.add(sb.toString());
+                    if (sb.length() > 0) {
+                        map.put(sb.toString(), 1);
+                    }
                 }
             }
-            ArrayList<String> arrayList = new ArrayList<>(set);
-
-            sortArray(arrayList);
-            logger.info("{}", arrayList);
         } catch (IOException e) {
             logger.warn("{}", e.getMessage());
         }
-    }
-
-    private static void sortArray(ArrayList<String> arrayList) {
-        for (int i = arrayList.size() - 1; i > 0; i--) {
-            int maxLoc = 0;
-            for (int j = 1; j <= i; j++) {
-                if (arrayList.get(j).compareTo(arrayList.get(maxLoc)) >= 0) {
-                    maxLoc = j;
-                }
-            }
-            String temp = arrayList.get(maxLoc);
-            arrayList.set(maxLoc, arrayList.get(i));
-            arrayList.set(i, temp);
+        for (var str : map.keySet()) {
+            logger.info("{}", str);
         }
     }
+
+//    private static void sortArray(HashMap<String, Integer> arrayList) {
+//        for (int i = arrayList.size() - 1; i > 0; i--) {
+//            int maxLoc = 0;
+//            for (int j = 1; j <= i; j++) {
+//                if (arrayList.get(j).compareTo(arrayList.get(maxLoc)) >= 0) {
+//                    maxLoc = j;
+//                }
+//            }
+//            String temp = arrayList.get(maxLoc);
+//            arrayList.set(maxLoc, arrayList.get(i));
+//            arrayList.set(i, temp);
+//        }
+//    }
 }
